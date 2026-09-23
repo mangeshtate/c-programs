178bfb9e1f42e8bae580778cc1d543413915692a
mangeshtate <mangeshtatepatil29@gmail.com>
2026-09-23 20:11:43 +0530

    Added Reportcard program

---
+++ Beginner/reportcard.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int studentID=10;// Write studentID here
+  float score=45.5;// Write score here
+  char grade='A';// Write grade here
+
+  printf("StudentID is %d\n",studentID);// Print studentID
+  printf("Score is %f\n",score);// Print score
+  printf("Grade is %c\n",grade);// Print grade
+
+  return 0;
+}
