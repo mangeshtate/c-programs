1209b50037ca4bb88224338c33130997a45f14d3
mangeshtate <mangeshtatepatil29@gmail.com>
2026-09-25 12:34:57 +0530

    Added Programs

---
+++ Operators/Boolean.c
@@ -0,0 +1,7 @@
+#include<stdio.h>//Booleans are the basis for all comparisons and conditions.
+int main(){
+    int voteage=18;
+    int age=25;
+    printf("%d",age>=voteage);//1 output as its true,0 for Flase
+    return 0;
+}
